3ba22f302518182a7a1078447490068874d096c2
Bo <yakxitech@gmail.com>
2013-10-09 21:47:50 -0400

    oic

--- src/socialNetworkMinning/twi.py
+++ src/socialNetworkMinning/twi.py
@@ -1,6 +1,6 @@
 import twitter 
 
-twitter_search = twitter.Twitter(domain=“search.twitter.com”)
+twitter_search = twitter.Twitter(domain="search.twitter.com")
 trends = twitter_search.trends()
 [trends['name'] for trend in trends['trends']]
 
